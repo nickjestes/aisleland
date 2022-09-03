b3e15fd05eea8d010df3afe74540b258a064eee9
Zane Sharif <Zane.Sharif@gmail.com>
2022-09-03 11:45:00 -0700

    on the page. just a safe starting ground

--- src/components/MultiStoreComp.js
+++ src/components/MultiStoreComp.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from "react";
 import '../styles/Header.css';
 import { useParams } from "react-router-dom";
+import Table from 'react-bootstrap/Table';
 
 
 function MultiStoreComp() {
     const {store, zipCode} = useParams()
+    const [storesList, setStores] = useState(null)
+
+
     console.log(store, zipCode)
     useEffect(()=>{
             fetch(`http://localhost:3001/api/stores/${store}/${zipCode}`,{
@@ -16,11 +20,29 @@ function MultiStoreComp() {
             return res.json()
         }).then(data=>{
             console.log(data)
+            setStores(data)
         })
     },[])
 
     return (
-        <h1>test</h1>
+                storesList && storesList.map((storeEntry) => (
+                <Table key={storeEntry.id}>
+                    {/* <thread>
+                        <th className='text-center'>Store Name</th>
+                        <th className='text-center'>Store Address</th>
+                    </thread> */}
+                    <tr>
+                        <td className='text-center'>
+                            {storeEntry.name}
+                            </td>
+                        <td className='text-center'>
+                            {storeEntry.address}
+                            </td>
+                        <button>Select Store</button>
+                    </tr>
+        
+                </Table>
+            ))
     );
   }
 
