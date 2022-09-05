d85f3b70760b044e96784680561c80cb007e3560
Zane Sharif <Zane.Sharif@gmail.com>
2022-09-05 15:15:24 -0700

    over adding

--- src/components/AuthForm/Index.js
+++ src/components/AuthForm/Index.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
-
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 export default function AuthForm(props) {
     const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [userName, setUserName] = useState("")
 
   const submit = e=>{
     e.preventDefault();
@@ -13,9 +14,10 @@ export default function AuthForm(props) {
     <div className = "AuthForm">
         <header>{props.type}</header>
         <form onSubmit = {submit}>
-            <input value={email} placeholder= "email" onChange={e=>setEmail(e.target.value)}/>
-            <input value={password} type="password" onChange={e=>setPassword(e.target.value)}/>
-            <button>{props.type}</button>
+            <input value={email} placeholder= "Enter Email" onChange={e=>setEmail(e.target.value)}/>
+            <input value={password} placeholder="Enter Password" type="password" onChange={e=>setPassword(e.target.value)}/>
+            {/* <input value={userName} placeholder="Enter Username" onChange={e=>setUserName(e.target.value)}/> */}
+            <Link to="/"><button>{props.type}</button></Link>
         </form>
     </div>
   )
